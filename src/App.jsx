2e1719efb20bcc17399c3cee6c06a1edbce7076f
manya_sh2 <2005.sharma.manya@gmail.com>
2025-08-21 04:51:34 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+      
     </div>
   )
 }
